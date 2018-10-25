bc0498795a61a5d3d020883d56cc6bc241696b85
LIU Shuai <sunnyjava@hotmail.com>
2018-10-25 22:00:54 +0800

    message with nickname; broadcast messages to all sessions.

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
+  //res.send('<h1>Hello world lattimore</h1>');
+
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit('chat message', 'a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+
+    io.emit('chat message', 'a user leave');
+  });
+
   socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+
     io.emit('chat message', msg);
   });
 });
